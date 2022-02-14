357955a57caf8a13756831a0ff511b57a62f2a8e
tonysprocket <andrew_noel@live.com>
2022-02-13 22:29:01 -0700

    compile, setup and deploy

---
+++ scripts/script_utils.py
@@ -0,0 +1,114 @@
+import sys
+
+sys.path.append("./")
+sys.path.append("../")
+
+import subprocess
+import os
+import dotenv
+import json
+import asyncio
+from contracts.lib.openzeppelin.tests.utils import Signer
+
+dotenv.load_dotenv()
+
+
+def owner_account():
+    return os.path.join(
+        os.path.dirname(__file__),
+        "../current_state_info/current_account.json",
+    )
+
+
+def pool():
+    return os.path.join(
+        os.path.dirname(__file__),
+        "../current_state_info/current_deployment_info.json",
+    )["POOL"]
+
+
+def router():
+    return os.path.join(
+        os.path.dirname(__file__),
+        "../current_state_info/current_deployment_info.json",
+    )["ROUTER"]
+
+
+def ercs():
+    return os.path.join(
+        os.path.dirname(__file__),
+        "../current_state_info/fake_ercs.json",
+    )["ROUTER"]
+
+
+def signer():
+    return Signer(int(os.getenv("PRIV_KEY")))
+
+
+def create_invoke_command(
+    address, name_of_contract, function_name, input_list, signature
+):
+    cmd_list = [
+        f"starknet",
+        f"invoke",
+        f"--address",
+        f"{address}",
+        f"--abi",
+        f"interfaces/{name_of_contract}_abi.json",
+        f"--function",
+        f"{function_name}",
+        f'--network={os.getenv("STARKNET_NETWORK")}',
+        f"--inputs",
+    ]
+
+    for i in input_list:
+        cmd_list.append(str(i))
+
+    cmd_list.extend([f"--signature", f"{signature[0]}", f"{signature[1]}"])
+
+    return " ".join(cmd_list)
+
+
+def create_deploy_command(name_of_contract, input_list):
+    cmd_list = [
+        f"starknet",
+        f"deploy",
+        f"--contract",
+        f"builds/{name_of_contract}_compiled.json",
+        f'--network={os.getenv("STARKNET_NETWORK")}',
+        f"--inputs",
+    ]
+
+    for i in input_list:
+        cmd_list.append(str(i))
+
+    return " ".join(cmd_list)
+
+
+def create_compile_command(name_of_contract, name_of_compiled):
+    cmd_list = [
+        f"starknet-compile",
+        f"contracts/{name_of_contract}.cairo",
+        f"--output",
+        f"builds/{name_of_compiled}_compiled.json",
+        f"--abi",
+        f"interfaces/{name_of_compiled}_abi.json",
+    ]
+    return " ".join(cmd_list)
+
+
+def run_command(cmd):
+    output = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    if output.stdout:
+        output = str(output.stdout).replace(":", "\n").split("\n")
+
+        address = output[2].strip(" ").strip("\n")
+        tx_hash = output[4].strip(" ").strip("\n")
+        return address, tx_hash
+    else:
+        return None
+
+
+def write_result_to_storage(result, file_name):
+    with open(f"current_state_info/{file_name}.json", "w") as file:
+        file.write(str(result))
---
+++ scripts/setup_account.py
@@ -0,0 +1,21 @@
+from script_utils import (
+    create_compile_command,
+    create_deploy_command,
+    write_result_to_storage,
+    run_command,
+    signer,
+)
+
+account_path = "contracts.lib.openzeppelin.Account"
+account = "Account"
+signer = signer()
+
+c_cmd = create_compile_command(account_path, account)
+x = run_command(c_cmd)
+
+
+d_cmd = create_deploy_command(account, [signer.public_key])
+a, t = run_command(d_cmd)
+
+result = {"address": a, "tx": t}
+write_result_to_storage(result, "current_account")
