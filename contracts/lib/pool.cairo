bd1a75fa0dfd1017fa15d8e62e20f1d8dda365e0
Tony Sprocket <tonysprocket@gmail.com>
2022-07-07 15:34:44 -0600

    upgrade repo and scripts to use nile

--- contracts/lib/pool.cairo
+++ contracts/lib/pool.cairo
@@ -3,10 +3,11 @@
 from starkware.cairo.common.cairo_builtins import HashBuiltin
 from starkware.starknet.common.syscalls import get_caller_address, get_contract_address
 from starkware.cairo.common.uint256 import Uint256
+from starkware.cairo.common.bool import TRUE, FALSE
 
 from openzeppelin.access.ownable import Ownable
-from contracts.lib.bool import TRUE, FALSE
-from contracts.lib.Pool_base import Pool_deposit, Pool_withdraw
+
+from contracts.lib.Pool_base import Pool
 
 @constructor
 func constructor{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}(router : felt):
@@ -16,18 +17,18 @@ end
 
 @external
 func deposit{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}(
-        amount : Uint256, address : felt, erc20_address : felt) -> (success : felt):
+        amount : Uint256, user_address : felt, erc20_address : felt) -> (success : felt):
     Ownable.assert_only_owner()
 
-    Pool_deposit(amount, address, erc20_address)
+    Pool.deposit(amount, user_address, erc20_address)
     return (TRUE)
 end
 
 @external
 func withdraw{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}(
-        amount : Uint256, address : felt, erc20_address : felt) -> (success : felt):
+        amount : Uint256, user_address : felt, erc20_address : felt) -> (success : felt):
     Ownable.assert_only_owner()
 
-    Pool_withdraw(amount, address, erc20_address)
+    Pool.withdraw(amount, user_address, erc20_address)
     return (TRUE)
 end
--- scripts/approve_pool.py
+++ scripts/approve_pool.py
@@ -1,71 +1,32 @@
-import asyncio
-import json
-import dotenv
 import os
-from script_utils import (
-    create_invoke_command,
-    run_command,
-    write_result_to_storage,
-    owner_account,
-    router,
-    pool,
-    ercs,
-    signer,
-)
-from script_utils import DECIMALS
+import sys
 
-from starknet_py.contract import Contract
-from starknet_py.net.client import Client
-from starkware.cairo.common.hash_state import compute_hash_on_elements
-from starkware.crypto.signature.signature import (
-    private_to_stark_key,
-    sign,
-)
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
 
-dotenv.load_dotenv()
+from scripts.script_utils import DECIMALS, MAX_FEE
 
-owner_address = json.load(open(owner_account()))["address"]
-router_address = json.load(open(router()))["ROUTER"]["address"]
-pool_address = json.load(open(pool()))["POOL"]["address"]
-ercs = [x["address"] for x in json.load(open(ercs()))]
-nonce = 0
+def run(nre):
+    # get user account
+    user_account = nre.get_or_deploy_account("BALLER")
 
+    # get ERC contracts
+    erc_list = tZWBTC, _ = nre.get_deployment("tZWBTC")
+    tUSDC, _ = nre.get_deployment("tUSDC")
+    tETH, _ = nre.get_deployment("tETH")
 
-async def approve_pool(owner_address, router_address, pool_address, ercs, nonce):
-    key = signer().private_key
+    # get pool adddress
+    pool_address, _ = nre.get_deployment("mammoth_pool")
 
-    i_i = [pool_address, 400000*DECIMALS]
+    # approve pool to spend args
+    approve_args = [int(pool_address, 16), 400000*DECIMALS]
 
-    account_contract = await Contract.from_address(owner_address, Client("testnet"))
+    # send tx
+    tx_one = user_account.send(to=tZWBTC, method='approve', calldata=approve_args, max_fee=MAX_FEE)
+    tx_two = user_account.send(to=tUSDC, method='approve', calldata=approve_args, max_fee=MAX_FEE)
+    tx_three = user_account.send(to=tETH, method='approve', calldata=approve_args, max_fee=MAX_FEE)
 
-    for erc in ercs:
-        erc_contract = await Contract.from_address(erc, Client("testnet"))
-
-        # (nonce,) = await account_contract.functions["get_nonce"].call()
-        nonce = nonce + 1
-        selector = erc_contract.functions["approve"].get_selector(
-            "approve")
-        print(selector)
-        calldata = i_i
-        calldata_len = len(calldata)
-
-        message = [
-            erc_contract.address,
-            selector,
-            0,
-            2,
-        ]
-        message_hash = compute_hash_on_elements(message)
-        public_key = private_to_stark_key(key)
-        signature = sign(msg_hash=message_hash, priv_key=key)
-
-        input_list = [message] + calldata + [nonce]
-
-        cmd = create_invoke_command(
-            owner_address, "Account", "__execute__", input_list, signature
-        )
-        a, t = run_command(cmd)
-
-
-asyncio.run(approve_pool(owner_address,
-            router_address, pool_address, ercs, nonce))
+    print(tx_one)
+    print(tx_two)
+    print(tx_three)
